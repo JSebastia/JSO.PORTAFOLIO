b953caca24380c07ba7472b85d1aafc58a9e42d1
JSebastia <osoriosebastian748@gmail.com>
2026-01-29 19:38:30 -0500

    Se agrego un nuevo diseño a la pagina de inicio

--- evento.js
+++ evento.js
@@ -1,5 +1,5 @@
 //Capturar evento para organizar barra de navegación en dispositivos móbiles. 
-const hamburgerMenu = document.querySelector(".headerPag_nav_span");
+/*const hamburgerMenu = document.querySelector(".headerPag_nav_span");
 const nav = document.querySelector(".headerPag_nav_div");
 
 hamburgerMenu.addEventListener("click", () => {
@@ -10,4 +10,36 @@ hamburgerMenu.addEventListener("click", () => {
     } else{
         hamburgerMenu.innerHTML = "☰";
     }    
+});*/
+
+const elementImage = document.getElementById("imgAnimacion");
+console.log(elementImage.getBoundingClientRect().y);
+
+function animacionCaja1() {
+    elementImage.style.transition = "1.0s"
+    elementImage.style.boxShadow = "0px 10px 100px  #FCA31180";
+}
+
+function animacionCaja2() {
+    elementImage.style.transition = "1.0s"
+    elementImage.style.boxShadow = "0px 10px 30px  #FCA31180";
+}
+        
+setInterval(animacionCaja1, 1000)
+setInterval(animacionCaja2, 2000)
+
+function changeDesignHeader() {
+
+    const designHeader = document.getElementById("header-id");
+
+    if (scrollY > 0) {
+        designHeader.classList.add("design-header-scroll");
+    } else {
+        designHeader.classList.remove("design-header-scroll");
+    }
+}
+
+document.addEventListener("scroll", function () {
+    changeDesignHeader();
 });
+        
